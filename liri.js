8dae299d8adfac8162404eb98aee9fa8c8a05320
Steve Mustanski <steve.mustanski@gmail.com>
2018-10-08 17:42:52 -0400

    added grabing the 2nd argument as the thing to search for in the spotify code

--- liri.js
+++ liri.js
@@ -5,22 +5,26 @@ var Spotify = require('node-spotify-api');
 //console.log(keys.spotify.id);
 //console.log(keys.spotify.secret);
 
-
 //console.log(keys.omdb.secret);
 
+const command = process.argv[2];
+const thingToGet = process.argv[3];
 
- 
+console.log(command);
+console.log(thingToGet);
+
+// set up new spotify object for node-spotify-api
 var spotify = new Spotify({
   id: keys.spotify.id,
   secret: keys.spotify.secret
 });
  
-spotify.search({ type: 'track', query: 'Run to the Hills' }, function(err, data) {
+spotify.search({ type: 'track', query: thingToGet }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
  
-//console.log(data.tracks.items[0]); 
+console.log(data.tracks.items[0]); 
 console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
 console.log(`Title: ${data.tracks.items[0].name}`);
 console.log(`Album: ${data.tracks.items[0].album.name}`);
