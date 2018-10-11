c3ea11d85e87b998d4ab00c4778cdd8c6148726b
Steve Mustanski <steve.mustanski@gmail.com>
2018-10-11 16:38:21 -0400

    split out get concert info into function and added process of concerts in random file

--- liri.js
+++ liri.js
@@ -24,6 +24,10 @@ if (command === 'do-what-it-says') {
     if (command === 'movie-this') {
       moveIt(thingToGet);
     }
+    if (command === 'concert-this') {
+      let thingToGetFix = thingToGet.slice(1, -1);
+      concertIt(thingToGetFix);
+    }
 
   });
 }
@@ -38,7 +42,63 @@ if (command === 'movie-this') {
 
 // get a list of events from bands in town if the command is 'concert-this'
 if (command === 'concert-this') {
-  let url = `https://rest.bandsintown.com/artists/${thingToGet}/events?app_id=${keys.bands.id}`;
+  concertIt(thingToGetFix);
+}
+
+// function to run spotify package and print out information
+function spotifyIt(song) {
+  // set up new spotify object for node-spotify-api
+  var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
+
+  // search spotify using the api
+  spotify.search({ type: 'track', query: song }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    } else if (data.tracks.items[0] != undefined) {
+      // console loge out the results from the first item in the array
+      console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
+      console.log(`Title: ${data.tracks.items[0].name}`);
+      console.log(`Album: ${data.tracks.items[0].album.name}`);
+      console.log(`Preview: ${data.tracks.items[0].preview_url}`);
+    } else {
+      console.log(`Track: '${song}' not found`)
+
+    }
+  });
+}
+
+// fucntion to get movie information and print it out to console
+function moveIt(title) {
+  // default to 'Mr. Nobody' if no movie is specified
+  if (!title) {
+    title = 'Mr. Nobody';
+  }
+  let url = `http://www.omdbapi.com/?apikey=${keys.omdb.secret}&t=${title}`;
+  request(url, function (error, response, body) {
+
+    // If the request was successful...
+    if (!error && response.statusCode === 200) {
+
+      // Then log the body from the site!
+      let movie = JSON.parse(body);
+      console.log(`Title: ${movie.Title}`);
+      console.log(`Year: ${movie.Year}`);
+      console.log(`IMDB Rating: ${movie.imdbRating}`);
+      console.log(`Rotten Tomatoes: ${movie.Ratings[1].Value}`);
+      console.log(`Country: ${movie.Country}`);
+      console.log(`Language: ${movie.Language}`);
+      console.log(`Plot: ${movie.Plot}`);
+      console.log(`Actors: ${movie.Actors}`);
+    }
+  });
+}
+
+// fucntion to get concert information
+function concertIt(artist) {
+  let url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=${keys.bands.id}`;
   request(url, function (error, response, body) {
 
     // If the request was successful...
@@ -48,7 +108,7 @@ if (command === 'concert-this') {
       let bands = JSON.parse(body);
       // check for no events
       if (bands.length === 0) {
-        console.log(`Sorry there are no upcomming events for ${thingToGet}`);
+        console.log(`Sorry there are no upcomming events for ${artist}`);
       }
       // loop through the results
       for (let i = 0; i < bands.length; i++) {
@@ -56,7 +116,7 @@ if (command === 'concert-this') {
         console.log(`Location: ${bands[i].venue.city}, ${bands[i].venue.region}`);
 
         // modify format of incomming date time
-        let justDate = bands[i].datetime.substring(0,10);
+        let justDate = bands[i].datetime.substring(0, 10);
         let printDate = convertDate(justDate);
 
         console.log(`Date: ${printDate}`)
@@ -64,68 +124,6 @@ if (command === 'concert-this') {
       }
     }
   });
-}
-
-if (command === 'do-what-it-says') {
-  fs.readFile('random.txt', 'utf8', (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-    data = data.split(',');
-    console.log(data);
-
-  });
-}
-
-// function to run that spotify package and print out information
-function  spotifyIt(song) {
-    // set up new spotify object for node-spotify-api
-    var spotify = new Spotify({
-      id: keys.spotify.id,
-      secret: keys.spotify.secret
-    });
-  
-    // search spotify using the api
-    spotify.search({ type: 'track', query: song }, function (err, data) {
-      if (err) {
-        return console.log('Error occurred: ' + err);
-      } else if (data.tracks.items[0] != undefined) {
-        // console loge out the results from the first item in the array
-        console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
-        console.log(`Title: ${data.tracks.items[0].name}`);
-        console.log(`Album: ${data.tracks.items[0].album.name}`);
-        console.log(`Preview: ${data.tracks.items[0].preview_url}`);
-      } else {
-        console.log(`Track: '${song}' not found`)
-  
-      }
-    });
-}
-
-function moveIt(title) {
-    // default to 'Mr. Nobody' if no movie is specified
-    if (!title) {
-      title = 'Mr. Nobody';
-      console.log(thingToGet);
-    }
-    let url = `http://www.omdbapi.com/?apikey=${keys.omdb.secret}&t=${title}`;
-    request(url, function (error, response, body) {
-  
-      // If the request was successful...
-      if (!error && response.statusCode === 200) {
-  
-        // Then log the body from the site!
-        let movie = JSON.parse(body);
-        console.log(`Title: ${movie.Title}`);
-        console.log(`Year: ${movie.Year}`);
-        console.log(`IMDB Rating: ${movie.imdbRating}`);
-        console.log(`Rotten Tomatoes: ${movie.Ratings[1].Value}`);
-        console.log(`Country: ${movie.Country}`);
-        console.log(`Language: ${movie.Language}`);
-        console.log(`Plot: ${movie.Plot}`);
-        console.log(`Actors: ${movie.Actors}`);
-      }
-    });
 }
 
 // converts the incomming date string to the requested format
